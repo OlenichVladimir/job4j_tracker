25f37155f092391ae5ddfbdbd539bcdb4880ffea
OlenichVladimir <absolut0409@gmail.com>
2023-07-13 09:19:30 +0800

    1.5. Поля объекта.

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,34 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("Как завут нашего котика и какую еду предпочитает котенок?");
+        Cat gav = new Cat();
+        gav.eat("У котика по имени Гав любимая еда это котлеты");
+        gav.giveNick("У нашего котика имя Гав");
+        gav.show();
+        System.out.println("Как завут нашего второго кота и какую еду он предпочитает");
+        Cat black = new Cat();
+        black.eat("У нашего кота по имени Черныш любимая еда это рыба");
+        black.giveNick("У нашего второго кота имя Черныш");
+        black.show();
     }
 }
