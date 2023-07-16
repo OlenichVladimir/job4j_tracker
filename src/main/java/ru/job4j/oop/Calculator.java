81acf4f48cc54f535db89a91dd583b41c551077c
OlenichVladimir <absolut0409@gmail.com>
2023-07-16 12:22:00 +0800

    1.9. Статические и нестатические методы.

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
     }
 
     public static int minus(int b) {
-        return x - b;
+        return b - x;
     }
 
     public int multiply(int a) {
@@ -17,7 +17,7 @@ public class Calculator {
     }
 
     public int divide(int c) {
-        return x / c;
+        return c / x;
     }
 
     public int sumAllOperation(int s) {
