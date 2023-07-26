519a28c8c163461f857f50b5b213a47de9a36b5a
OlenichVladimir <absolut0409@gmail.com>
2023-07-26 09:40:17 +0800

    3.0. Реализация класса StartUI. Вывод меню.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,17 +1,38 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import java.util.Scanner;
 
-public class StartUI extends Item {
+public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("EEE, MMMM d, yyyy");
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("E, MMMM d, y, G, w, a, HH:mm:ss:SS", Locale.ENGLISH);
-        System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(formatter));
-        System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(formatter1));
-        System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(formatter2));
-        System.out.println(item);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
