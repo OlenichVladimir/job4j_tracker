bd85029c668187e82b6af423d9fba8e8713641b0
OlenichVladimir <absolut0409@gmail.com>
2023-10-09 12:21:21 +0800

    0. Map, HashMap.

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        Map<String, String> mail = new HashMap<>();
+        mail.put("ivanov@gmail.com", "Ivanov I.I.");
+        mail.put("petrov@gmail.com", "Petrov P.P.");
+        mail.put("sidorov@gmail.com", "Sidorov S.S.");
+        for (String key : mail.keySet()) {
+            String value = mail.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
